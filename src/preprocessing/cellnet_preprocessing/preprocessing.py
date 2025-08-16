b9cebf3760f2bd3c631e25e32f474c0636f22456
Javier Martin <jmartinpizarro04@gmail.com>
2025-08-16 18:38:19 +0200

    feat: refactor entire preprocessing

--- src/preprocessing/cellnet_preprocessing/preprocessing.py
+++ src/preprocessing/cellnet_preprocessing/preprocessing.py
@@ -1,11 +1,10 @@
 "Preprocessing for the CellNet"
 
-import os
-import shutil
 import logging
 
 import pandas as pd
-from PIL import Image
+
+import src.preprocessing.cellnet_preprocessing.utils as utils
 
 pd.set_option("display.max_columns", None)
 
@@ -16,15 +15,7 @@ def preprocess_dataset(input_path: str, output_path: str, neural_network: str) -
     Return 0 if everything went correct, -1 otherwise
     """
 
-    try:
-        if os.path.exists(output_path) and os.path.isdir(output_path):
-            shutil.rmtree(output_path)
-        os.makedirs(f"{output_path}/images")
-    except FileNotFoundError:
-        logging.error(
-            f"[preprocess_dataset]::An error has ocurred when creating the output path in {output_path}. Ensure that the path exists.\n"
-        )
-        return -1
+    utils.generate_output_folders(output_path)
 
     # Data Processing Algorithm
     # AS images are 300x300, it is not necessary to take into account
@@ -47,49 +38,8 @@ def preprocess_dataset(input_path: str, output_path: str, neural_network: str) -
     for index, row in images_metadata.iterrows():
         fileName, size, matrix_str = row["filename"], row["size"], row["matrix_inline"]
 
-        # each image has a 300x300 size, it is possible to know the
-        # size of each cell just doing dividing
-        # total image size / size of matrix
-        with Image.open(f"{input_path}/images/{fileName}") as im:
-            width, height = im.size
-
-            cell_size = width / size
-
-            # now it is possible to go through the entire image, iterating
-            # from left to right, upper to lower part of it until all cells
-            # has been processed
-
-            # iterate through the total number of cells
-            matrix_str = list(matrix_str)
-            matrix_str = [ch for ch in matrix_str if ch != ","]
-            c = 1
-            while c <= size * size:
-                # awesome way of calculating row and col of a matrix!
-                row = (c - 1) // size
-                col = (c - 1) % size
-
-                # define bounding box of the new image
-                left_origin = col * cell_size
-                upper_origin = row * cell_size
-                right_offset = left_origin + cell_size
-                lower_offset = upper_origin + cell_size
-                box = (left_origin, upper_origin, right_offset, lower_offset)
-
-                label = matrix_str[c - 1]
-
-                # crop and save
-                sub_img = im.crop(box)
-
-                # transform it into a 32x32 image size
-                sub_img = sub_img.resize((32, 32))
-
-                sub_img.save(f"{output_path}/images/{fileName}_{c}.jpeg")
-
-                output["fileName"].append(f"{fileName}_{c}.jpeg")
-                output["label"].append(label)
-
-                c += 1
+        utils.process_image(output, fileName, size, matrix_str, input_path, output_path)
 
     df = pd.DataFrame(output)
-    df.to_csv(f"{output_path}/preprocessed_dataset.csv")
+    df.to_csv(f"{output_path}/preprocessed_dataset.csv", index=False)
     return 0
---
+++ src/preprocessing/cellnet_preprocessing/utils.py
@@ -0,0 +1,94 @@
+"Utils for the CellNet preprocessing methodology"
+
+import os
+import shutil
+import logging
+from typing import Dict, List
+
+from PIL import Image
+
+
+def generate_output_folders(output_path: str) -> int:
+    """
+    Generates the output folders required for the preprocessing
+    script to run correctly
+
+    @param output_path: str -> name of the folder where the raw
+    data is going to be preprocessed
+
+    @returns 0 if all went correctly. FileNotFoundError otherwise
+    """
+
+    try:
+        if os.path.exists(output_path) and os.path.isdir(output_path):
+            shutil.rmtree(output_path)
+        os.makedirs(f"{output_path}/images")
+    except FileNotFoundError:
+        logging.error(
+            f"[preprocess_dataset]::An error has ocurred when creating the output path in {output_path}. Ensure that the path exists.\n"
+        )
+
+    return 0
+
+
+def process_image(
+    output: Dict[List[str], List[str]],
+    fileName: str,
+    size: int,
+    matrix_str: str,
+    input_path: str,
+    output_path: str,
+) -> int:
+    """
+    Process an entire image depending on its size,
+    cropping it.
+
+    @param output: dictionary with image's metadata
+    @param fileName: str -> name of the input image
+    @param size: int -> size of the square-grid
+    @param matrix_str: str -> a simpler way of representing the matrix in a string; used for generating the labels
+    @param input_path: str -> input path of the raw data folder
+    @param output_path: str -> output path
+
+    @returns the 0 if everything went correct, -1 otherwise
+    """
+
+    # each image has a 300x300 size, it is possible to know the
+    # size of each cell just doing dividing
+    # total image size / size of matrix
+    with Image.open(f"{input_path}/images/{fileName}") as im:
+        width, height = im.size
+        cell_size = width / size
+
+        # now it is possible to go through the entire image, iterating
+        # from left to right, upper to lower part of it until all cells
+        # has been processed iterate through the total number of cells
+
+        matrix_str = list(matrix_str)
+        matrix_str = [ch for ch in matrix_str if ch != ","]
+        c = 1
+
+        while c <= size * size:
+            # awesome way of calculating row and col of a matrix!
+            row = (c - 1) // size
+            col = (c - 1) % size
+
+            # define bounding box of the new image
+            left_origin = col * cell_size
+            upper_origin = row * cell_size
+            right_offset = left_origin + cell_size
+            lower_offset = upper_origin + cell_size
+            box = (left_origin, upper_origin, right_offset, lower_offset)
+            label = matrix_str[c - 1]
+            # crop and save
+            sub_img = im.crop(box)
+            # transform it into a 32x32 image size
+            sub_img = sub_img.resize((32, 32))
+
+            sub_img.save(f"{output_path}/images/{fileName}_{c}.jpeg")
+
+            output["fileName"].append(f"{fileName}_{c}.jpeg")
+            output["label"].append(label)
+            c += 1
+
+    return 0
